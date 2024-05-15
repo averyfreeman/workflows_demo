c569cb51ce4c56291e008a01c3c8b40e65a11282
Avery Freeman <averyfreeman@gmail.com>
2024-05-14 23:41:39 -0700

    raise exception

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-#!/usr/bin/env python3
 import sys
-from art import text2art
 
-def main(text):
-    return print(str(text2art(text, font="random")))
-
-hello = "hello world"
+def main():
+    print("Hello World! From Python: " + str(sys.version_info))
+    if sys.version_info >= (3, 10) and sys.version_info < (3, 11):
+        # let's make this script fail for Python 3.9
+        raise Exception('Python version 3.10.x is unsupported!')
 
 if __name__ == "__main__":
-    main(hello)
+    main()
